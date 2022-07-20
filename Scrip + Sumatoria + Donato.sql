70aae22764583fa8329f74d5955c6ceea16b3ec7
Bruno <102097102+BrunoMDonato@users.noreply.github.com>
2022-07-20 13:48:32 -0300

    Update Scrip + Sumatoria + Donato.sql

--- Scrip + Sumatoria + Donato.sql
+++ Scrip + Sumatoria + Donato.sql
@@ -189,3 +189,90 @@ WHERE trabajo.id_trabajo = chequeo.id_trabajo = producto.id_trabajo;
 
 SELECT * 
 FROM completo;
+
+
+
+-- FUNCTIONS --
+
+
+delimiter $$
+CREATE FUNCTION `auto_cliente`(par_id_cliente INT) 
+RETURNS char(255)
+READS SQL DATA
+BEGIN
+	declare resultado char (255);
+    set resultado = (select modelo from auto where id_cliente = par_id_cliente);
+RETURN resultado;
+END$$
+
+delimiter $$
+CREATE FUNCTION `revision`(id_auto_p INT) 
+RETURNS char(255)
+READS SQL DATA
+BEGIN
+	declare var1 int;
+    declare var2 int;
+    set var1 = (select id_trabajo from trabajo where id_auto_p = id_auto);
+    set var2 = (select revisado from chequeo where var1 = id_trabajo);
+    if (var2) > 0 then
+		return ('El auto fue chequeado');
+	else
+		return ('El auto no fue chequeado');
+	end if; 
+    
+END$$
+
+
+
+--Stored procedures --
+
+
+CREATE PROCEDURE `order_by` (in var char(50), in var2 int)
+BEGIN
+	if var2 = 0 then
+		set @orden = concat ('order by ', var, ' desc');
+    else
+		set @orden = concat ('order by ', var, ' asc');
+    end if;
+		set @final = concat ( 'select * from cliente ', @orden);
+    prepare runSQL from @final;
+    execute runSQL;
+    deallocate prepare runSQL;
+END
+
+
+CREATE PROCEDURE `sp_insert`(in nom char(200), in tel int, in p_dni int)
+BEGIN
+	insert into cliente ( nombre_apellido, telefono, dni ) values (nom, tel, p_dni);
+END
+
+
+--TRIGGERS--
+
+CREATE TABLE `insert_auto` (
+  `id_auto` int unsigned NOT NULL AUTO_INCREMENT,
+  `marca` varchar(200) NOT NULL,
+  `modelo` varchar(200) NOT NULL,
+  `kms` int unsigned NOT NULL,
+  `id_cliente` int unsigned NOT NULL,
+  PRIMARY KEY (`id_auto`)
+  ) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TRIGGER AFT_INS_auto
+AFTER INSERT ON auto
+FOR EACH ROW
+INSERT INTO insert_auto
+VALUES (NEW.id_auto, NEW.marca, NEW.modelo, NEW.kms, NEW.id_cliente);
+
+
+
+CREATE TABLE logs( 
+	fecha DATE,
+    hora TIME,
+    usuario VARCHAR(100));
+    
+CREATE TRIGGER BEF_DEL_CLIENTE_LOGS
+BEFORE DELETE ON cliente
+FOR EACH ROW
+INSERT INTO logs
+VALUES (CURDATE(), CURTIME(), USER());
