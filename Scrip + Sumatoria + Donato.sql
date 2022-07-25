80921bccaea18c58b6a765243e43301a10b80a20
Bruno <102097102+BrunoMDonato@users.noreply.github.com>
2022-07-25 17:47:20 -0300

    Update Scrip + Sumatoria + Donato.sql

--- Scrip + Sumatoria + Donato.sql
+++ Scrip + Sumatoria + Donato.sql
@@ -285,3 +285,25 @@ VALUES (CURDATE(), CURTIME(), USER());
    
 DELETE FROM cliente
 where id_cliente = 10
+
+
+
+--# SENTENCIAS // USUARIOS
+
+
+--# Utilizamos BBDD mysql
+Use mysql;
+
+--# Creamos usuarios
+create user bruno@localhost identified by 'Coder2022';
+
+create user martin@localhost identified by 'Coder_2022';
+
+--# Otorgamos perdmisos (1° lectura, modificacion, insersion), (2° solo lectura)
+grant select, update, insert on lubricentro.* to bruno@localhost;
+
+grant select on lubricentro.* to martin@localhost;
+
+--# Muestra los permisos otorgados
+show grants for martin@localhost;
+show grants for bruno@localhost;
